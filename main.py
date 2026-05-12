9a848bb92994f277782b6c0d05cc9653923c0c80
Alexander Conrad <conrad.alex55@gmail.com>
2026-05-12 14:12:13 -0500

    Updated README and started working on main code

---
+++ main.py
@@ -0,0 +1,16 @@
+# Notes for the functionality of the program
+# The program should:
+#   Allow the creation of new contacts (stored data is name, email, phone number, other forms of contact, and notes on the person)
+#   Allow the user to search for a contact by their name
+#   Allow the user to delete a contact
+#   Display all of the contacts
+#   Save all data in JSON files
+#
+# The program should NOT:
+#   Send JSON data back to me
+#   Corrupt files
+#   Perform actions without user consent
+#   Sell them ice cream
+
+import os
+import json
